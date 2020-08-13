8ea3c49cf9e814bdb0e2e9150f3898e5e6a5a193
rajesh <krajeshshahu@gmail.com>
2020-08-13 20:00:00 +0530

    Update MagicInEnum.cpp

--- Enum/MagicInEnum.cpp
+++ Enum/MagicInEnum.cpp
@@ -3,15 +3,20 @@
 using namespace std;
 
 //plain enum
-enum color{red,green,yellow,black};
-enum people{good,bad};
+//enum color{red,green,yellow,black};
+//enum people{good,bad};
+
+enum class color{red,green,yellow,black};
+enum class people{good,bad};
 
 int main()
 {
-    if(red == good) cout<<"Magic in enum"<<endl;
+    if(red == good) cout<<"Magic in enum"<<endl;//main.cpp:14:8: error: ‘red’ was not declared in this scope
     else cout<<"All is well in enum"<<endl;
     return 0;
 }
 /* OP
-Magic in enum                                                                   
+//Magic in enum                                                                   
+main.cpp:14:8: error: ‘red’ was not declared in this scope
+
 */
