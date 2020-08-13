ae4efd1c39e4366b01edadc4b8eac96cb5cb8d25
rajesh <krajeshshahu@gmail.com>
2020-08-13 19:39:07 +0530

    Create MagicInEnum.cpp

---
+++ Enum/MagicInEnum.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+//plain enum
+enum color{red,green,yellow,black};
+enum people{good,bad};
+
+int main()
+{
+    if(red == good) cout<<"Magic in enum"<<endl;
+    else cout<<"All is well in enum"<<endl;
+    return 0;
+}
+/* OP
+Magic in enum                                                                   
+*/
